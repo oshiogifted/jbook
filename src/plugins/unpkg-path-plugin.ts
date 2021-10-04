2ab1f3d3c63023036e48796e1dbf6d85e6c2f62e
oshiogifted <oshiogifted2011@gmail.com>
2021-10-04 11:21:55 -0400

    caching layer in indexedDB with localForage

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,20 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
+
+// IndexedDB via localforage
+const fileCache = localforage.createInstance( {
+	name: 'filecache'
+});
+
+// testing IndexedDB
+/* (async () => {
+	await fileCache.setItem('color', 'red');
+
+	const color = await fileCache.getItem('color');
+
+	console.log(color);
+})() //IIFE */
 
 export const unpkgPathPlugin = () => {
 	return {
@@ -30,21 +45,35 @@ export const unpkgPathPlugin = () => {
 					return {
 						loader: 'jsx',
 						contents: `
-              import React, { useState } from 'react';
+              import React, { useState } from 'react-select';
               console.log(React, useState);
             `
 					};
 				}
 
+				/*** Caching with key:value pairs using IndexedDB */
+				// Check to see if we have already fetched this file and if it is in the cache
+				// using args.path as key
+				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+				// If it is, return it immediately 
+				if (cachedResult) {
+					return cachedResult;
+				}
+				// If not, make a fetch and...
         // fetch pkg from unpkg using axios
 				const { data, request } = await axios.get(args.path);
 				//console.log("fetching from unpkg - ", data);
 				//console.log('axios response request -', request);
-				return {
+				const result: esbuild.OnLoadResult =  {
 					loader: 'jsx', // just so esbuild can understand that it may have to parse some jsx in the file
 					contents: data,
 					resolveDir: new URL('./', request.responseURL).pathname
 				};
+				
+				// ...store response in cache
+				await fileCache.setItem(args.path, result);
+
+				return result;
 			});
 		}
 	};
