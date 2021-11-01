d41921636365457fcb545a826df55494e49dbad8
oshiogifted <oshiogifted2011@gmail.com>
2021-11-01 19:20:50 -0400

    setting up react-resizable

---
+++ src/components/resizable.tsx
@@ -0,0 +1,10 @@
+import { ResizableBox } from 'react-resizable';
+
+interface ResizableProps {
+  direction: 'horizontal' | 'vertical';
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  return <>{children}</>;
+};
+export default Resizable;
